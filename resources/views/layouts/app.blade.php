d15f7631816eebcbf06ee824dfc905b53d8b97b2
StevensCanon11 <elian.canon@uniminuto.edu.co>
2025-03-08 23:40:41 -0500

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,11 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-
-
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="//unpkg.com/alpinejs" defer></script>
 
+    <!-- Alpine.js (usando npm en lugar de CDN) -->
+    @vite('resources/js/app.js')
 </head>
 
 <body class="font-sans antialiased">
